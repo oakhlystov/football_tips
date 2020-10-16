4f2b21d251a1e4ca623ba95a7105685f7f60ec97
okhlystov <footboom_moscow@mail.ru>
2020-10-16 17:47:10 +0300

    UserInterface.java - beautify

--- src/ftblApp_tips_v02/main/UserInterface.java
+++ src/ftblApp_tips_v02/main/UserInterface.java
@@ -15,9 +15,9 @@ public class UserInterface extends JPanel
         new UserInterface();
     }
 
-    JButton btn_1, btn_2, btn_3;
-    JTextField txtf_date, txtf_admin, txtf_champ;
-    JLabel lbl_date, lbl_admin, lbl_champ;
+    JButton btn_saveNewInfo, btn_validateFormInfo, btn_exit;
+    JTextField txtf_date, txtf_admin, txtf_champ, txtf_federation;
+    JLabel lbl_date, lbl_admin, lbl_champ, lbl_federation;
 
     public UserInterface()
     {
@@ -25,7 +25,7 @@ public class UserInterface extends JPanel
         JFrame mainFrame = new JFrame("football tips");
         mainFrame.setResizable(true);
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        mainFrame.setLayout(new GridLayout(20,2));
+        mainFrame.setLayout(new GridLayout(20,5));
         mainFrame.setSize(1000,1000);
         mainFrame.setVisible(true);
         mainFrame.setLocationRelativeTo(null);
@@ -34,30 +34,36 @@ public class UserInterface extends JPanel
         lbl_date = new JLabel("Дата:");
         txtf_date = new JTextField("EmptyDate");
 
-        lbl_champ = new JLabel("Администратор:");
-        txtf_champ = new JTextField("EmptyAdmin");
+        lbl_federation = new JLabel("Федерация:");
+        txtf_federation = new JTextField("EmptyFederation");
+
+        lbl_champ = new JLabel("Чемпионат:");
+        txtf_champ = new JTextField("EmptyChamp");
 
         lbl_admin = new JLabel("Администратор:");
         txtf_admin = new JTextField("EmptyAdmin");
 
-        btn_1 = new JButton("Сохранить");
-        btn_2 = new JButton("Проверить");
-        btn_3 = new JButton("Выход?");
+        btn_saveNewInfo = new JButton("Сохранить как новую запись");
+        btn_validateFormInfo = new JButton("Проверить правильность данных");
+        btn_exit = new JButton("Выход?");
 
         /* Заполнение формы элементами */
         mainFrame.add(lbl_date);
         mainFrame.add(txtf_date);
+        mainFrame.add(lbl_champ);
+        mainFrame.add(txtf_champ);
         mainFrame.add(lbl_admin);
-        mainFrame.add(btn_1);
-        mainFrame.add(btn_3);
+        mainFrame.add(txtf_admin);
+        mainFrame.add(btn_saveNewInfo);
+        mainFrame.add(btn_exit);
 
         /* ActionListener на кнопки */
-        btn_1.addActionListener(handler);
-        btn_2.addActionListener(handler);
-        btn_3.addActionListener(handler);
+        btn_saveNewInfo.addActionListener(handler);
+        btn_validateFormInfo.addActionListener(handler);
+        btn_exit.addActionListener(handler);
 
-        /* Валидация всех жлементов, которые должны быть на форме. По факту - позволяет отобразить все, что должно быть на форме */
-        mainFrame.revalidate();
+        /* Валидация всех элементов, которые должны быть на форме. По факту - позволяет отобразить все, что должно быть на форме */
+        mainFrame.validate();
         //mainFrame.pack(); //Собирает форму, в том числе подгоняет размер формы под содержимое (игнор mainFrame.setSize())
 
 
@@ -66,25 +72,26 @@ public class UserInterface extends JPanel
     public class Listener implements ActionListener {
         public void actionPerformed(ActionEvent e) {
             try {
-                if (e.getSource() == btn_1)
+                if (e.getSource() == btn_saveNewInfo)
                 {
 
-
-                    JOptionPane.showMessageDialog(null, txtf_date.getText());
                     DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH-mm-ss");
                     String requiredDate = df.format(new Date());
                     Writer.writeToExcel(txtf_date.getText(), txtf_date.getText()+"2", txtf_date.getText()+"3", requiredDate);
                     JOptionPane.showMessageDialog(null,"button 1 pressed");
                     System.out.println("DEBUG | button 1 pressed");
                 }
-                if (e.getSource() == btn_3)
+                if (e.getSource() == btn_exit)
                 {
+                    /* Тестовые данные для проверки работы нажатия по кнопке*/
                     DateFormat df = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss");
                     String requiredDate = df.format(new Date());
-                    JOptionPane.showMessageDialog(null,requiredDate);
+                    JOptionPane.showMessageDialog(null, requiredDate);
+                    /* Конец тестовых данных */
                 }
             } catch (Exception ex) {
                 JOptionPane.showMessageDialog(null,e.getActionCommand()+"\nError");
+                System.out.println("Error in try/catch in ActionListener => ActionPerformed: \n"+System.err);
             }
 
         }
