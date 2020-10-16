9fcf2b61de3e6612efc46f484090edc4fad7e114
okhlystov <footboom_moscow@mail.ru>
2020-10-16 09:57:09 +0300

    add UserInterface.java

---
+++ src/ftblApp_tips_v02/main/UserInterface.java
@@ -0,0 +1,39 @@
+package ftblApp_tips_v02.main;
+
+import javax.swing.*;
+import java.awt.event.*;
+
+public class UserInterface extends JFrame
+{
+    JButton btnTEST;
+
+
+    eHandler handler = new eHandler();
+
+    public UserInterface (String s)
+    {
+        super(s);
+
+
+
+        pack();
+    }
+
+
+
+    public class eHandler implements ActionListener {                                                                //действие при нажатии на вещицу
+        public void actionPerformed(ActionEvent e) {
+            //if (b == "0"){t1.setText("");} else if (b==null) {t1.setText("");}
+            System.out.println(e.getActionCommand());
+            try
+            {
+                //123123@TODO-1
+                if (e.getSource() == btnTEST) {
+                    System.out.println("Hello");}
+                //123123@TODO-1
+            } catch (Exception ex) {
+                JOptionPane.showMessageDialog(null, "Error");
+            }
+        }
+    }
+}
