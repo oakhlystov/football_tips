1518ae493124ec4e2d14a3c9989a7c382b391992
okhlystov <footboom_moscow@mail.ru>
2020-10-16 10:39:10 +0300

    changed architector mind

--- src/ftblApp_tips_v02/main/Main.java
+++ src/ftblApp_tips_v02/main/Main.java
@@ -1,15 +1,20 @@
 package ftblApp_tips_v02.main;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Main {
     public static void main(String[] args)
     {
-        UserInterface ui = new UserInterface("Tips (v0.2)");
-        ui.setVisible(true);
-        ui.setSize(600,600);
-        ui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        ui.setResizable(true);
-        ui.setLocationRelativeTo(null);
+        JFrame frame = new JFrame("aw hello");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new GridLayout(3,2));
+        frame.add(new JButton("jBTN1"));
+        frame.add(new JLabel("hey"));
+        frame.setLocationRelativeTo(null);
+        frame.setSize(700,700);
+        frame.setResizable(true);
+        frame.pack();
+        frame.setVisible(true);
     }
 }
---
+++ src/ftblApp_tips_v02/main/Maths.java
@@ -0,0 +1,6 @@
+package ftblApp_tips_v02.main;
+
+public class Maths
+{
+
+}
